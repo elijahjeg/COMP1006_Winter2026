805be05f1c34aca42583330199e43fc5f3b1c6ee
Elijah <elijah.jegede@mygeorgian.ca>
2026-01-24 10:24:39 -0500

    Added what I learned from Lab 2

--- 03/03_make_it_maintainable/index.php
+++ 03/03_make_it_maintainable/index.php
@@ -1,4 +1,11 @@
 <?php
+/*
+One thing I learned from this lab was the importance of file structure for scalability in PHP.
+Separating into different files makes it more easy to understand which code does what, and when the
+project gets bigger all I need to do is add another require and make a new file instead of having long
+messy code.
+*/
+
 require "header.php";
 
 require "dynamic_ul.php";
